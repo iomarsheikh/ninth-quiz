010f08f5c78e73a1e80ba50df020ac77a78e3242
Muhammad Omar Sheikh <omarsheikh076@gmail.com>
2023-11-08 02:09:34 +0500

    Add files via upload

---
+++ quiz.ts
@@ -0,0 +1,42 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+const apiLink: string = "https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple";
+
+let quizData = async (data: string) => {
+    let fetchData: any = await fetch(data);
+    let res = await fetchData.json();
+    return res.results;
+}
+
+let data = await quizData(apiLink);
+
+let startQuiz = async () => {
+    let score: number = 0;
+    let answers = await inquirer.prompt({
+        name: "name",
+        type: "input",
+        message: "Enter Name: "
+    });
+
+    for (let i = 0; i < 5; i++) {
+        let quizAns = [...data[i].incorrect_answers, data[i].correct_answer];
+
+        let question = await inquirer.prompt({
+            name: "ans",
+            type: "list",
+            message: chalk.bold.blueBright(data[i].question),
+            choices: quizAns.map((val: any) => val),
+        });
+
+
+        if (question.ans == data[i].correct_answer) {
+            ++score;
+        }
+    }
+
+    console.log(chalk.italic.green(`${answers.name} Your Score is: ${score}`));
+
+}
+
+await startQuiz();
